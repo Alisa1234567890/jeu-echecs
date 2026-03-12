33ade4eb3bd011df34d05c706906afc3eafde38a
Alisa Rudenko <alisa223455@gmail.com>
2026-03-12 14:27:00 +0100

    DCA

--- src/main/java/org/model/plateau/Direction.java
+++ src/main/java/org/model/plateau/Direction.java
@@ -1,15 +1,21 @@
 package org.model.plateau;
 
-
 public enum Direction {
-    NORTH(-1, 0), SOUTH(1, 0), WEST(0, -1), EAST(0, 1),
-    NORTH_EAST(-1, 1), NORTH_WEST(-1, -1), SOUTH_EAST(1, 1), SOUTH_WEST(1, -1);
+    HAUT( -1, 0),
+    BAS( 1, 0),
+    GAUCHE(0, -1),
+    DROITE(0, 1),
+    HAUT_DROITE(-1, 1),
+    HAUT_GAUCHE(-1, -1),
+    BAS_DROITE(1, 1),
+    BAS_GAUCHE(1, -1);
 
     public final int dx;
     public final int dy;
 
-    Direction(int dx, int dy) {
-        this.dx = dx;
-        this.dy = dy;
+    Direction(int _dx, int _dy) {
+        // as requested: invert the signs of provided values
+        this.dx = -_dx;
+        this.dy = -_dy;
     }
 }
